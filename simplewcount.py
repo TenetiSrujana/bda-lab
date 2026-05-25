dcc151cc61f64164500b9cb0a694453e98937f66
Teneti Srujana <161198620+TenetiSrujana@users.noreply.github.com>
2026-05-25 23:14:35 +0530

    Update and rename wcount.py to simplewcount.py

--- simplewcount.py
+++ simplewcount.py
@@ -5,14 +5,35 @@ start-all.sh
 jps
 
 # Create input file
-nano input.txt
+nano input.txt -> hi haddop hello big data
 
 # Create mapper
-nano wcmap.py
-
+nano wcmap.py           
+      |
+      v
+#!/usr/bin/env python3
+import sys
+for line in sys.stdin:
+    words = line.strip().split()
+    for word in words:
+        print(word + "\t1")
+      
 # Create reducer
 nano wcred.py
-
+      |
+      v
+#!/usr/bin/env python3
+import sys
+word_count = {}
+for line in sys.stdin:
+    word, count = line.strip().split("\t")
+    if word in word_count:
+        word_count[word] += int(count)
+    else:
+        word_count[word] = int(count)
+for word in word_count:
+    print(word + "\t" + str(word_count[word]))
+  
 # Give permissions
 chmod +x wcmap.py
 chmod +x wcred.py
