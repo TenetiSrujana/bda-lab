1f54636028d1d0fdd8281081bdfb465d45267135
Teneti Srujana <161198620+TenetiSrujana@users.noreply.github.com>
2026-05-26 01:30:59 +0530

    Update sparkstreaming.py

--- sparkstreaming.py
+++ sparkstreaming.py
@@ -26,6 +26,32 @@ query = result.writeStream \
     .format("console") \
     .start()
 query.awaitTermination()
+
+or (og below)
+    
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum
+from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType
+spark = SparkSession.builder \
+    .appName("RetailStoreStreaming") \
+    .getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
+schema = StructType([
+    StructField("ProductID", IntegerType(), True),
+    StructField("Amount", DoubleType(), True)])
+input_path = "/home/hadoop/spark_input"
+retail_stream_df = spark.readStream \
+    .format("csv") \
+    .option("header", "true") \
+    .schema(schema) \
+    .load(input_path)
+processed_df = retail_stream_df.groupBy("ProductID") \
+    .agg(sum("Amount").alias("TotalSales"))
+query = processed_df.writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .start()
+query.awaitTermination()
 ......
 
 ~/spark-3.5.2-bin-hadoop3/bin/spark-submit sparkstream.py
